0541b8a0cfc9c7a89051cf1d193f082f0083c958
RafaelGSS <rafael.nunu@hotmail.com>
2020-09-07 00:38:18 -0300

    docs: add examples to grow with source code

---
+++ examples/fastify.js
@@ -0,0 +1,11 @@
+require('../')()
+const fastify = require('fastify')({ logger: true })
+
+fastify.get('/', function (request, reply) {
+  reply.send('works!')
+})
+
+fastify.listen(3000, '0.0.0.0', function (err) {
+  if (err) throw err
+})
+
