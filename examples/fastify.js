5a1d99e861ce2b1921056d262a48b1339ebeba6f
RafaelGSS <rafael.nunu@hotmail.com>
2020-10-25 16:41:25 -0300

    update: async context

--- examples/fastify.js
+++ examples/fastify.js
@@ -1,7 +1,11 @@
-require('../')().start()
+const agent = require('../')();
+agent.start();
+
 const fastify = require('fastify')({ logger: true })
 
 fastify.get('/', function (request, reply) {
+  const store = agent._instrumentator.trace.getStore();
+  console.log(store)
   reply.send('works!')
 })
 
