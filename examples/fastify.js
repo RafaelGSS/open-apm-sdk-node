077b8e66614b2c451295ab0ee567e6024f92b1b1
RafaelGSS <rafael.nunu@hotmail.com>
2020-10-28 15:20:35 -0300

    fix(span): adjust to create subsegment

--- examples/fastify.js
+++ examples/fastify.js
@@ -4,10 +4,25 @@ agent.start();
 const fastify = require('fastify')({ logger: true })
 
 fastify.get('/', function (request, reply) {
-  const store = agent._instrumentator.trace.getStore();
-  console.log(store)
+  console.log(agent._instrumentator.trace.getStore().name)
+  agent._instrumentator.wrapOnTrace(shouldTraceFunction.bind(this, 'example'));
+  console.log(agent._instrumentator.trace.getStore().name)
+  agent._instrumentator.wrapOnTrace(anotherFunction);
+  console.log(agent._instrumentator.trace.getStore().name)
   reply.send('works!')
 })
+
+function shouldTraceFunction(param) {
+  agent._instrumentator.wrapOnTrace(shouldTrace2Function);
+}
+
+function shouldTrace2Function() {
+
+}
+
+function anotherFunction() {
+
+}
 
 fastify.listen(3000, '0.0.0.0', function (err) {
   if (err) throw err
