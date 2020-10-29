486d9cd03dc4eb4b80b785be5d91c84317ded7e7
RafaelGSS <rafael.nunu@hotmail.com>
2020-10-28 23:42:43 -0300

    feat: add wrapper to async operations

--- examples/fastify.js
+++ examples/fastify.js
@@ -4,24 +4,40 @@ agent.start();
 const fastify = require('fastify')({ logger: true })
 
 fastify.get('/', function (request, reply) {
-  console.log(agent._instrumentator.trace.getStore().name)
   agent._instrumentator.wrapOnTrace(shouldTraceFunction.bind(this, 'example'));
-  console.log(agent._instrumentator.trace.getStore().name)
   agent._instrumentator.wrapOnTrace(anotherFunction);
-  console.log(agent._instrumentator.trace.getStore().name)
   reply.send('works!')
 })
 
 function shouldTraceFunction(param) {
   agent._instrumentator.wrapOnTrace(shouldTrace2Function);
 }
+function shouldTrace2Function() { }
 
-function shouldTrace2Function() {
+function anotherFunction() { }
 
+fastify.get('/async', async function (request, reply) {
+  await Promise.all([
+    agent._instrumentator.wrapOnTraceAsync(asyncFunc1),
+    agent._instrumentator.wrapOnTraceAsync(asyncFunc3)
+  ]);
+  reply.send('works!');
+});
+
+async function asyncFunc1() {
+  await sleep(5000);
+  await agent._instrumentator.wrapOnTraceAsync(asyncFunc2);
 }
+async function asyncFunc2() { }
 
-function anotherFunction() {
+async function asyncFunc3() {
+  await sleep(5000);
+  await agent._instrumentator.wrapOnTraceAsync(asyncFunc4);
+}
+async function asyncFunc4() { }
 
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 fastify.listen(3000, '0.0.0.0', function (err) {
